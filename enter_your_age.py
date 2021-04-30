4501a7bfc2279c45c74191c7caf8c8e5c9b8a963
klcatha <klcatha0518@gmail.com>
2021-05-01 01:47:21 +0900

    first commit

---
+++ enter_your_age.py
@@ -0,0 +1,8 @@
+age = input("enter your age:")
+int_age = int(age)
+if int_age < 20:
+    print("お前未成年だろ")
+else:
+    print("酒を飲め")
+    
+    
---
+++ for.py
@@ -0,0 +1,6 @@
+random = []
+for i in range(10):
+    random.append(100)
+
+print(random)
+
