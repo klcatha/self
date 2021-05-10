7e94e4bc2b9b53013b9df851f22a3dbc22d0239f
klcatha <klcatha0518@gmail.com>
2021-05-10 21:46:24 +0900

    add dict value

--- dict.py
+++ dict.py
@@ -1,3 +1,12 @@
-fruits = {"Apple":"Red",
-          "Banana":"Yellow"}
-print(fruits)
+facts = dict()
+
+# バリューの追加と参照
+
+facts["code"] = "fun"
+print(facts["code"])
+
+facts["Bill"]= "Gates"
+print(facts["Bill"])
+
+facts["founded"] = 1776
+print(facts["founded"])
